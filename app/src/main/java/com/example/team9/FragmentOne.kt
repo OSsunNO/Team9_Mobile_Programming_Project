4e7210fd2766a8c5e59f225063ffb358d1210688
Sunho <1020sunho@naver.com>
2022-11-22 23:36:33 +0900

    fragment에 구글 맵 띄우고 마커 및 위도 경도 설정 완료

--- app/src/main/java/com/example/team9/FragmentOne.kt
+++ app/src/main/java/com/example/team9/FragmentOne.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentManager
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.protobuf.MapFieldLite
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,7 +31,7 @@ class FragmentOne : Fragment(), OnMapReadyCallback {
     private var param1: String? = null
     private var param2: String? = null
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var mMap: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,15 +46,16 @@ class FragmentOne : Fragment(), OnMapReadyCallback {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_one, container, false)
+        mMap = rootView.findViewById(R.id.mapview) as MapView
+        mMap.onCreate(savedInstanceState)
+        mMap.getMapAsync(this)
+        return rootView
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val supportFragmentManager = getParentFragmentManager()
-        val mapFragment: SupportMapFragment? = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-    }
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//    }
 
     companion object {
         @JvmStatic
@@ -66,10 +70,48 @@ class FragmentOne : Fragment(), OnMapReadyCallback {
 
     // 내가 사용할 수 있는 Map이 GoogleMap 파라미터를 통해 전달
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-        val place = LatLng(35.241615, 128.695587)
+        // 경도/위도 기반 위치 저장
+        val place = LatLng(37.5663, 126.9779)
+        // 구글맵에 띄울 마커 변수 정의
         val marker = MarkerOptions().position(place).title("마커입니다.")
-        mMap.addMarker(marker)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(place))
+        googleMap.addMarker(marker)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place,12f))
+        val cameraOption = CameraPosition.builder().target(place).zoom(15.0f).build()
+        // 카메라 위치
+        // val camera = CameraUpdateFactory.newLatLng(place)
+
+//        val camera = CameraUpdateFactory.newCameraPosition(cameraOption)
+
+        // mMap.clear()
+        // 마커를 구글맵에 추가
+    }
+    override fun onStart() {
+        super.onStart()
+        mMap.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mMap.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mMap.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mMap.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mMap.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mMap.onDestroy()
     }
 }
